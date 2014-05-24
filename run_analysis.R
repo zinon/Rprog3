ac4d4ced68652cc909e24336aa7d495827e9a173
zinon <zinon123@gmail.com>
2014-05-24 16:07:17 +0200

    helper functions + comments

--- run_analysis.R
+++ run_analysis.R
@@ -4,12 +4,16 @@
 dataDir <- "./data"
 outFile <- "processedData.txt"
 ##_-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-_
+# first initialization
+kFeaturesRead <- FALSE
+dfFeatures <- data.frame()
+##_-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-_
 main <- function(){
 
 }
 ##_-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-_
-getFeaturesCache <- function(){
-
+getFeaturesCache <- function(){ #save CPU time!! Use cached data frame for features for multiple calls
+    
   if (kFeaturesRead) { info("getting features from cache"); return(dfFeatures)}
  
   info("getting features first");
@@ -18,36 +22,35 @@ getFeaturesCache <- function(){
   
 }
 ##_-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-_
-getFeatures <- function(){
+getFeatures <- function(){ # returns a data frame of features
 
   info("getting interesting features")
   
   d <- extractFeatures( readFeatures() )
 }
 ##_-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-_
-extractFeatures <- function(df){
+extractFeatures <- function(df){ #isolate the columns which have only interesting features (mean and std)
 
   info("extracting interesting features")
-  #names of subset columns required
-  pattern <- "mean\\(\\)|std\\(\\)"
-  df[ grep(pattern, df$V2), ] # df$V2 = df[,2]
+  pattern <- "mean\\(\\)|std\\(\\)"   #names of subset columns required
+  df[ grep(pattern, df$V2), ] # or df$V2 = df[,2]
 }
 ##_-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-_
-readFeatures <- function(){
+readFeatures <- function(){#read all info from the features file and return a table
 
   info("reading all features")
   filepath <- file.path(dataDir, "features.txt")
   dtable <- read.table(filepath)
 }
 ##_-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-_
-getActivities <- function(){
+getActivities <- function(){ #get the activities data
 
   info("reading all activities labels")
   filepath <- file.path(dataDir, "activity_labels.txt")
   dtable <- read.table(filepath)
 }
 ##_-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-_
-getDataFileName <- function( dtype = "train", dcont = ""){
+getDataFileName <- function( dtype = "train", dcont = ""){#obtain data file with the appropriate name
 
   dcontent <- if ( dcont == "set") 
     "X_"	
@@ -62,7 +65,7 @@ getDataFileName <- function( dtype = "train", dcont = ""){
 
 }
 ##_-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-__-'`'-_
-getDataFile <- function(dtype = "train", dcont = ""){
+getDataFile <- function(dtype = "train", dcont = ""){ #get file with full path
 
   filepath <- file.path(dataDir, dtype, getDataFileName(dtype, dcont))
 
